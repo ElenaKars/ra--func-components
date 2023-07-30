9c3dacf299a9ac4b975ce1bf7d3583e39d86b4d3
Elena Karsten <e.karstenzv@gmail.com>
2023-07-31 00:46:05 +0800

    changed ShowItemFunc.jsx

--- src/component/ShowItemFunc.jsx
+++ src/component/ShowItemFunc.jsx
@@ -1,16 +1,16 @@
 export function ShopItemFunc ({product}){
     return (
-        <div class="main-content">
+        <div className="main-content">
   <h2>{product.brand}</h2>
   <h1>{product.title}</h1>
   <h3>{product.description}</h3>
-  <div class="description">
+  <div className="description">
     {product.descriptionFull}
   </div>
-  <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-  <div class="divider"></div>
-  <div class="purchase-info">
-    <div class="price">{product.currency}{product.price.toFixed(2)}</div>
+  <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+  <div className="divider"></div>
+  <div className="purchase-info">
+    <div className="price">{product.currency}{product.price.toFixed(2)}</div>
     <button>Добавить в корзину</button>
   </div>
 </div>
